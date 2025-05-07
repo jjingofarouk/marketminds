95a5c461cfd014fa36a6699a48cff43934bed3d7
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-07 21:52:59 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL
-  }
+  // Exclude server directory from build
+  exclude: ['../server/**/*'],
 }
 
 module.exports = nextConfig
