bb8f64965e6268498e20a43642e3780d8bc54541
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-07 14:16:54 +0300

    Create Watchlist.tsx

---
+++ src/components/Watchlist.tsx
@@ -0,0 +1,21 @@
+import { FC, useEffect } from 'react'
+import { useStockStore } from 'store/useStockStore'
+import StockCard from './StockCard'
+
+const Watchlist: FC = () => {
+  const { watchlist, fetchWatchlist } = useStockStore()
+
+  useEffect(() => {
+    fetchWatchlist()
+  }, [fetchWatchlist])
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {watchlist.map((stock) => (
+        <StockCard key={stock.symbol} stock={stock} />
+      ))}
+    </div>
+  )
+}
+
+export default Watchlist
