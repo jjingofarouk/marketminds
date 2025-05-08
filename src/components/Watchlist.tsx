3820565e5e4bbfb3dd126d19f522834b6ffbb45e
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-08 03:12:19 +0300

    Update Watchlist.tsx

--- src/components/Watchlist.tsx
+++ src/components/Watchlist.tsx
@@ -1,3 +1,4 @@
+// Watchlist.tsx
 import { FC, useEffect } from 'react'
 import { useStockStore } from '../store/useStockStore'
 import StockCard from './StockCard'
@@ -10,10 +11,18 @@ const Watchlist: FC = () => {
   }, [fetchWatchlist])
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {watchlist.map((stock) => (
-        <StockCard key={stock.symbol} stock={stock} />
-      ))}
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-4 sm:p-6">
+      {watchlist.length > 0 ? (
+        watchlist.map((stock) => (
+          <StockCard key={stock.symbol} stock={stock} />
+        ))
+      ) : (
+        <div className="col-span-full text-center py-12">
+          <p className="text-gray-400 text-lg font-medium">
+            Your watchlist is empty. Add some stocks to track!
+          </p>
+        </div>
+      )}
     </div>
   )
 }
